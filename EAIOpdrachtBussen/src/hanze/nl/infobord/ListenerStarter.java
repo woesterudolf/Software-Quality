53ce470c5088627024ae38cf25e6314430f54bc0
woesterudolf <rudolfklijnhout@hotmail.com>
2018-12-21 10:14:06 +0100

    intial commit

---
+++ EAIOpdrachtBussen/src/hanze/nl/infobord/ListenerStarter.java
@@ -0,0 +1,18 @@
+package hanze.nl.infobord;
+
+import javax.jms.Connection;
+import javax.jms.Destination;
+import javax.jms.ExceptionListener;
+import javax.jms.JMSException;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+public  class ListenerStarter implements Runnable, ExceptionListener {
+	//TODO	Implementeer de starter voor de messagelistener:
+//	Zet de verbinding op met de messagebroker en start de listener met 
+//	de juiste selector.
+
+}
---
+++ EAIOpdrachtBussen/src/hanze/nl/infobord/QueueListener.java
@@ -0,0 +1,13 @@
+package hanze.nl.infobord;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+public class QueueListener implements MessageListener {
+	//TODO 	implementeer de messagelistener die het bericht ophaald en
+//			doorstuurd naar verwerkBericht van het infoBord.
+//			Ook moet setRegels aangeroepen worden.
+	}
+
